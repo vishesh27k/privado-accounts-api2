2aac306dbeca05a86169f4ffc84b264c4ef5d418
vaibhavantil2 <88091026+vaibhavantil2@users.noreply.github.com>
2024-04-05 12:06:07 -0400

    Create S3stub.java

---
+++ src/main/java/ai/privado/demo/accounts/thirdparty/S3stub.java
@@ -0,0 +1,37 @@
+package your.package.name;
+
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+import software.amazon.awssdk.services.s3.model.S3Exception;
+import java.nio.file.Path;
+
+public class S3Service {
+    
+    private final S3Client s3;
+
+    public S3Service() {
+        this.s3 = S3Client.builder()
+                          .region(Region.YOUR_REGION) // Set your preferred region
+                          .credentialsProvider(DefaultCredentialsProvider.create())
+                          .build();
+    }
+
+    public void uploadFile(String bucketName, String key, Path file) {
+        try {
+            PutObjectRequest putObjectRequest = PutObjectRequest.builder()
+                                                                .bucket(bucketName)
+                                                                .key(key)
+                                                                .build();
+            s3.putObject(putObjectRequest, file);
+            System.out.println("File uploaded successfully to S3 bucket: " + bucketName);
+        } catch (S3Exception e) {
+            System.err.println(e.awsErrorDetails().errorMessage());
+        }
+    }
+
+    public void close() {
+        s3.close();
+    }
+}
