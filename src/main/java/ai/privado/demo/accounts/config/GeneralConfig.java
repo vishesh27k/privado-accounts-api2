9df8083e524b2464a45b79df11bfe1980d96ff21
Vishesh <45128646+vishesh27k@users.noreply.github.com>
2024-05-19 19:00:45 +0530

    Update GeneralConfig.java

--- src/main/java/ai/privado/demo/accounts/config/GeneralConfig.java
+++ src/main/java/ai/privado/demo/accounts/config/GeneralConfig.java
@@ -53,7 +53,6 @@ public class GeneralConfig {
 		DriverManagerDataSource dataSource = new DriverManagerDataSource();
 		dataSource.setDriverClassName(Objects.requireNonNull(env.getProperty("spring.datasource.driver-class-name")));
 		dataSource.setUrl(env.getProperty("accounts.datasource.url"));
-		dataSource.setUsername(env.getProperty("accounts.datasource.username"));
 		dataSource.setPassword(env.getProperty("accounts.datasource.password"));
 
 		return dataSource;
